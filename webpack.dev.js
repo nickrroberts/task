7a474a52b505ad508d70ebacd452703b0481ca17
Nick Roberts <9039456+nickrroberts@users.noreply.github.com>
2025-03-20 15:08:33 -0600

    style fixes for mobile type size

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: './src',
     hot: true,
     watchFiles: ['./src/**/*'],
     liveReload: true, 
