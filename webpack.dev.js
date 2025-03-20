31a8b385f5536dfd9d0ec48fbf0f1157185a053f
Nick Roberts <9039456+nickrroberts@users.noreply.github.com>
2025-03-20 13:53:05 -0600

    unstick build

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: './src',
+    static: './dist',
     hot: true,
     watchFiles: ['./src/**/*'],
     liveReload: true, 
